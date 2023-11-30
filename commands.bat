eaca77b281e5f4744d9da168c2eb0e1048762302
ChiragBolakani <cheragbolakani@gmail.com>
2023-11-30 13:48:45 +0530

    removed some lines

--- commands.bat
+++ commands.bat
@@ -1,12 +1,6 @@
 python get_setup_names.py ant
 python get_setup_data_json.py ant
-@REM python get_input_data.py
 python get_input_data_with_reason.py
 python label_encoding.py
 python vectorizer.py
 python fit_knn_vectorized.py
-@REM python fit_knn.py
-@REM python fit_dct.py
-@REM python fit_svc.py
-@REM python result_analysis.py
-@REM python empty_directory.py
