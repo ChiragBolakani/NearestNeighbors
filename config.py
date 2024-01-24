bd71d0ebc4610787591aff532d9b49e2ac9588fa
ChiragBolakani <cheragbolakani@gmail.com>
2024-01-24 15:22:05 +0530

    added config file

---
+++ config.py
@@ -0,0 +1,8 @@
+# Database config values
+CHENNAI_DB = "regression_db"
+ANTWERP_DB = "regression_db_ant"
+HOST = "localhost"
+
+# KNN training config values
+K_VALUE = 7
+ALGORITHM_FOR_NEIGHBORS = 'ball_tree'
