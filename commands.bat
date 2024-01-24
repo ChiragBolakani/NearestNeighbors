401b815fc3e0909d99206b2c2ad4a9627dc45505
ChiragBolakani <cheragbolakani@gmail.com>
2024-01-24 15:19:08 +0530

    cmd line args

--- commands.bat
+++ commands.bat
@@ -1,6 +1,7 @@
-python get_setup_names.py chn
-python get_setup_data_json.py chn
+python get_setup_names.py %1
+python get_setup_data_json.py %1 %2 %3
 python get_input_data_with_reason.py
 python label_encoding.py
 python vectorizer.py
 python fit_knn_vectorized.py
+python empty_directory.py
